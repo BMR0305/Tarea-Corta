3f75bbc213fc98733f0f084a37a63079a8e67ade
Kearranza <cbkevin.ant@gmail.com>
2020-10-16 19:11:55 -0600

    Clases registro y factura

--- src/cr/ac/itcr/trabajo/extraclase/Factura.java
+++ src/cr/ac/itcr/trabajo/extraclase/Factura.java
@@ -1,4 +1,41 @@
 package cr.ac.itcr.trabajo.extraclase;
 
 public class Factura {
+
+    private int monto;
+    private Object vehiculo;
+    private Object perfil;
+
+    public Factura(int monto, Object vehiculo, Object perfil) {
+        this.monto = monto;
+        this.vehiculo = vehiculo;
+        this.perfil = perfil;
+    }
+
+    public int getMonto() {
+        return monto;
+    }
+
+    public void setMonto(int monto) {
+        this.monto = monto;
+    }
+
+    public Object getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(Object vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public Object getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(Object perfil) {
+        this.perfil = perfil;
+    }
+    public void enviar(){
+        //codigo para enviar la factura
+    }
 }
--- src/cr/ac/itcr/trabajo/extraclase/Registro.java
+++ src/cr/ac/itcr/trabajo/extraclase/Registro.java
@@ -1,4 +1,74 @@
 package cr.ac.itcr.trabajo.extraclase;
 
 public class Registro {
+    private String correo;
+    private int numeroCedula;
+    private int telefono;
+    private String fechaVencimiento;
+    private String direccion;
+    private String licencia;
+
+    public Registro(String correo, int numeroCedula, int telefono, String fechaVencimiento, String direccion, String licencia) {
+        this.correo = correo;
+        this.numeroCedula = numeroCedula;
+        this.telefono = telefono;
+        this.fechaVencimiento = fechaVencimiento;
+        this.direccion = direccion;
+        this.licencia = licencia;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getNumeroCedula() {
+        return numeroCedula;
+    }
+
+    public void setNumeroCedula(int numeroCedula) {
+        this.numeroCedula = numeroCedula;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getFechaVencimiento() {
+        return fechaVencimiento;
+    }
+
+    public void setFechaVencimiento(String fechaVencimiento) {
+        this.fechaVencimiento = fechaVencimiento;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getLicencia() {
+        return licencia;
+    }
+
+    public void setLicencia(String licencia) {
+        this.licencia = licencia;
+    }
+
+    public void verificar(){
+        //codigo que verifica el correo de la persona
+    }
+    public void registrar(){
+        //codigo que ingresa a la persona al sistema
+    }
 }
